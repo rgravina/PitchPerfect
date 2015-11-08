d7e47cb4d9241b73f48f1fd431223c726a4f4b1d
Robert Gravina <robert.gravina@gmail.com>
2015-11-08 14:25:55 +0900

    Starting with centered microphone button.
    Using PDF export of PNG.

---
+++ PitchPerfact/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PitchPerfact
+//
+//  Created by Robert Gravina on 11/6/15.
+//  Copyright © 2015 Robert Gravina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
