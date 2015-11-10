68bf58b590c03c035453a4a848611eb41a0ebe31
Robert Gravina <robert.gravina@gmail.com>
2015-11-10 22:44:34 +0900

    Added view controller for playing sounds with button to play sound slowly.
    Action is empty for now.

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Robert Gravina on 11/10/15.
+//  Copyright © 2015 Robert Gravina. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+  @IBAction func playSoundSlowly(sender: UIButton) {
+  }
+}
