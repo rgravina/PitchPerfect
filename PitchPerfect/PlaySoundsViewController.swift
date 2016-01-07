3282972ca627f1ce3891b9aee3fb14f95d436671
Robert Gravina <robert.gravina@kii.com>
2016-01-07 13:25:57 +0900

    Play sound at a higher pitch.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -12,10 +12,14 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
   var audioPlayer:AVAudioPlayer!
   var recievedAudio:RecordedAudio!
+  var audioEngine:AVAudioEngine!
+  var audioFile:AVAudioFile!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     audioPlayer = try! AVAudioPlayer(contentsOfURL: recievedAudio.filePathURL)
+    audioFile = try! AVAudioFile(forReading: recievedAudio.filePathURL)
+    audioEngine = AVAudioEngine()
   }
 
   @IBAction func playSoundSlowly(sender: UIButton) {
@@ -35,7 +39,23 @@ class PlaySoundsViewController: UIViewController {
   }
 
   @IBAction func playChipmunkAudio(sender: UIButton) {
-
+    playAudioWithVariablePitch(1000)
+  }
+  
+  func playAudioWithVariablePitch(pitch: Float) {
+    audioPlayer.stop()
+    audioEngine.stop()
+    audioEngine.reset()
+    let audioPlayerNode = AVAudioPlayerNode();
+    audioEngine.attachNode(audioPlayerNode)
+    let changePitchEffect = AVAudioUnitTimePitch()
+    changePitchEffect.pitch = pitch
+    audioEngine.attachNode(changePitchEffect)
+    audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+    audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+    audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+    try! audioEngine.start()
+    audioPlayerNode.play()
   }
 
   @IBAction func stopPlayingSound(sender: UIButton) {
